6d14236ca2c0ccf6a89f824420da3209087a4031
TimoWieme <1004149@hr.nl>
2023-05-19 16:18:04 +0200

    add persona placeholder + "gewenste situatie"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: 'class',
   theme: {
+    extend: {
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif']
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
